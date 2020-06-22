3ad544b25a10b6a211a5c44ad2646024082e73a1
Nua07 <59082853+Nua07@users.noreply.github.com>
2020-06-22 11:22:24 +0900

    Add more examples

--- exam.py
+++ exam.py
@@ -19,7 +19,14 @@ class MyClass(client.Client):
 
         if chat.message == "와":
             await chat.reply("샌주")
-
+        
+        #자신 메시지만
+        if chat.message == ".삭제":
+            await chat.delete()
+        
+        #OpenChat 권한 있을떄
+        if chat.message == ".가리기":
+            await chat.hide()
 
 client = MyClass("DEVICE NAME")
 client.run("id", "pw")
