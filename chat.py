4e05e95cad01e86b5c712771237aadd34eacffae
Nua07 <admin@nua07.tk>
2020-06-23 14:50:56 +0900

    reply to sendText, reply message reply

--- chat.py
+++ chat.py
@@ -2,6 +2,8 @@ import asyncio
 import packet
 from bson import BSON as bson
 import time
+import json
+
 class Chat:
     def __init__(self, writer, packet):
         self.writer = writer
@@ -12,15 +14,35 @@ class Chat:
 
         self.type = self.rawBody["chatLog"]["type"]
         self.message = self.rawBody["chatLog"]["message"]
-
+        
+        self.authorId = self.rawBody["chatLog"]["authorId"]
         if "attachment" in self.rawBody["chatLog"]:
             self.attachment = self.rawBody["chatLog"]["attachment"]
+        else:
+            self.attachment = {}
 
         if "li" in self.rawBody:
             self.li = self.rawBody["li"]
+        else:
+            self.li = 0
+
         self.nickName = self.rawBody["authorNickname"]
-        
-    async def reply(self, msg, extra = "{}", t = 1):
+
+    async def reply(self, msg, type = 0):
+        return await self.sendText(msg,
+                json.dumps({
+                    "attach_only":False,
+                    "attach_type":type,
+                    "mentions":[],
+                    "src_linkId":self.li,
+                    "src_logId":self.logId,
+                    "src_mentions":[],
+                    "src_message":self.message,
+                    "src_type":self.type,
+                    "src_userId":self.authorId
+                    }), 26)
+
+    async def sendText(self, msg, extra = "{}", t = 1):
         return await self.writer.sendPacket(packet.Packet(0, 0, "WRITE", 0, bson.encode({
             "chatId": self.chatId,
             "extra": extra,
