4bf9b5de5c904fabe0edf1fbb54453695caf9b1a
PositivePeriod <positiveperiod.dev@gmail.com>
2021-02-14 01:26:50 +0900

    Organize channel
    Add function sendPacket

--- channel.py
+++ channel.py
@@ -10,71 +10,71 @@ class Channel:
         self.li = li
         self.writer = writer
 
-    async def sendChat(self, msg, extra, t):
-        return (await self.writer.sendPacket(packet.Packet(0, 0, "WRITE", 0, bson.encode({
-                "chatId": self.chatId,
-                "extra": extra,
-                "type": t,
-                "msgId": time.time(),
-                "msg": str(msg),
-                "noSeen": False,
-                })))).toJsonBody()
+    async def sendPacket(self, command, data):
+        packet = packet.Packet(0, 0, command, 0, bson.encode(data))
+        return (await self.writer.sendPacket(packet)).toJsonBody()
 
-    async def forwardChat(self, msg, extra, t):
-        return (await self.writer.sendPacket(packet.Packet(0, 0, "FORWARD", 0, bson.encode({
-                "chatId": self.chatId,
-                "extra": extra,
-                "type": t,
-                "msgId": time.time(),
-                "msg": str(msg),
-                "noSeen": False,
-                })))).toJsonBody()
+    async def sendChat(self, msg, extra, t):
+        data = {
+            "chatId": self.chatId,
+            "extra": extra,
+            "type": t,
+            "msgId": time.time(),
+            "msg": str(msg),
+            "noSeen": False,
+        }
+        return await self.sendPacket("WRITE", data)
 
     async def sendText(self, msg):
         return await self.sendChat(msg, "{}", 1)
 
+    async def forwardChat(self, msg, extra, t):
+        data = {
+            "chatId": self.chatId,
+            "extra": extra,
+            "type": t,
+            "msgId": time.time(),
+            "msg": str(msg),
+            "noSeen": False,
+        }
+        return await self.sendPacket("FORWARD", data)
+
     async def deleteMessage(self, logId):
-        return (await self.writer.sendPacket(packet.Packet(0, 0, "DELETEMSG", 0, bson.encode({
-                "chatId": self.chatId,
-                "logId": logId
-                })))).toJsonBody()
+        data = {"chatId": self.chatId, "logId": logId}
+        return await self.sendPacket("DELETEMSG", data)
 
     async def hideMessage(self, logId, t):
         if self.li:
-            return (await self.writer.sendPacket(packet.Packet(0, 0, "REWRITE", 0, bson.encode({
+            data = {
                 "c": self.chatId,
                 "li": self.li,
                 "logId": logId,
                 "t": t
-            })))).toJsonBody()
+            }
+            return await self.sendPacket("REWRITE", data)
 
     async def kickMember(self, mid):
         if self.li:
-            await (self.writer.sendPacket(packet.Packet(0, 0, "KICKMEM", 0, bson.encode({
+            data = {
                 "li": self.li,
                 "c": self.chatId,
                 "mid": mid,
-            })))).toJsonBody()
+            }
+            await self.sendPacket("KICKMEM", data)
 
     async def setMeta(self, t, content):
-        return (await self.writer.sendPacket(packet.Packet(0, 0, "SETMETA", 0, bson.encode({
-                "chatId": self.chatId,
-                "type": t,
-                "content": content
-                })))).toJsonBody()
+        data = {
+            "chatId": self.chatId,
+            "type": t,
+            "content": content
+        }
+        return await self.sendPacket("SETMETA", data)
 
     async def getLinkInfo(self):
-        return (await self.writer.sendPacket(packet.Packet(0, 0, "INFOLINK", 0, bson.encode({
-                "lis": [self.li]
-                })))).toJsonBody()
+        return await self.sendPacket("INFOLINK", {"lis": [self.li]})
 
     async def getChatInfo(self):
-        return (await self.writer.sendPacket(packet.Packet(0, 0, "CHATINFO", 0, bson.encode({
-                "chatId": self.chatId
-                })))).toJsonBody()
+        return await self.sendPacket("CHATINFO", {"chatId": self.chatId})
 
     async def getUserInfo(self, userId):
-        return (await self.writer.sendPacket(packet.Packet(0, 0, "MEMBER", 0, bson.encode({
-                "chatId": self.chatId,
-                "memberIds": [userId]
-                })))).toJsonBody()
+        return await self.sendPacket("MEMBER", {"chatId": self.chatId, "memberIds": [userId]})
