da41d53d1a823cc3f2b67bcd48327d2d32bd3a15
andyyeyeye <63383967+andyyeyeye@users.noreply.github.com>
2021-02-15 13:37:53 +0900

    Update main.py

--- tests/main.py
+++ tests/main.py
@@ -2,16 +2,19 @@ from kakaopy.httpApi import RequestPasscode, RegisterDevice
 from .customClient import CustomClient
 
 def main():
-    user_id = str(input("ID : "))
-    user_pw = str(input("PW : "))
-    device_name = "KAKAOPY TEST"
-    user_uuid = 'AA'  # TODO 되는 경우에 대한 규칙성 필요 => randomize
+    check_registered = str(input("Have you already registered? (y/n) "))
+    user_id = "your email"
+    user_pw = "your password"
+    device_name = "any string"
+    user_uuid = 'REVWSUNFMQ=='  # TODO 되는 경우에 대한 규칙성 필요 => randomize 바꾸지 않는게 좋을 듯
+    
+    if check_registered == n:
+        RequestPasscode(user_id, user_pw, device_name, user_uuid)
 
-    RequestPasscode(user_id, user_pw, device_name, user_uuid)
+        print("Please check your phone or computer for the authorization passcode")
+        passcode = str(input("PASSCODE : "))
 
-    print("Please check your phone or computer for the authorization passcode")
-    passcode = str(input("PASSCODE : "))
-    RegisterDevice(user_id, user_pw, device_name, user_uuid, passcode)
+        RegisterDevice(user_id, user_pw, device_name, user_uuid, passcode)
 
     client = CustomClient(device_name, user_uuid)
     client.run(user_id, user_pw)
