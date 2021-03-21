8972fd70a1bc938439835c1a8c29bc38167a0ab9
JongHyeon Lee <jhlee@bfy.kr>
2021-03-21 18:09:54 +0900

    fix error

--- tests/main.py
+++ tests/main.py
@@ -8,7 +8,7 @@ def main():
     device_name = "any string"
     user_uuid = 'REVWSUNFMQ=='  # TODO 되는 경우에 대한 규칙성 필요 => randomize 바꾸지 않는게 좋을 듯
     
-    if check_registered == n:
+    if check_registered == 'n':
         RequestPasscode(user_id, user_pw, device_name, user_uuid)
 
         print("Please check your phone or computer for the authorization passcode")
