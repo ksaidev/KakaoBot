a0dbd541a5119e6b32714672466992c2588d7c85
SnowSuno <percy3368@gmail.com>
2021-05-31 14:13:55 +0900

    Fix text

--- tests/customClient.py
+++ tests/customClient.py
@@ -1,5 +1,5 @@
 from kakaopy.client import Client
-
+import json
 
 class CustomClient(Client):
     async def onPacket(self, packet):
@@ -14,8 +14,9 @@ class CustomClient(Client):
             await chat.reply("KAKAOPY is running")
 
         if chat.message == "태그":
-            attachment = {'mentions': [{'user_id': chat.authorId, 'at': [1], 'len': 2}]} #이런 방식으로 메세지를 보내는 법은 깃헙 위키에 나와 있습니다
-            await chat.channel.sendChat("@태그",json.dumps(attachment),1) 
+            # 이와 같은 방식으로 메세지를 보내는 법은 깃헙 위키에 나와 있습니다
+            attachment = {'mentions': [{'user_id': chat.authorId, 'at': [1], 'len': 2}]}
+            await chat.channel.sendChat("@태그", json.dumps(attachment), 1)
             
         # 자신의 메시지의 경우
         if chat.message == ".삭제":
